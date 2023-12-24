593d7c4f7cc56d2d7c4aaa4c72c7e84aff6da94f
Jochen <rothjochen@gmail.com>
2023-12-24 02:09:14 +0100

    [TASK] Update alpine image, use schedule keepalive job

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@
 docker build --no-cache --progress=plain . -f Dockerfile -t ochorocho/franken-typo3:latest && docker run --rm -it -v `pwd`/test.sh:/tmp/test.sh --entrypoint "ash" ochorocho/franken-typo3:latest /tmp/test.sh
 docker images | grep ochorocho/franken-typo3
 
-# docker push ochorocho/gitpod-tdk:latest
 # docker run -v $PWD/typo3:/app -p 8100:80 -p 450:443  ochorocho/franken-typo3:latest
